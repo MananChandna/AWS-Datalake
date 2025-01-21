d7bf1c8a532d8a220ff7de9d281198ba3e271abf
Manan Chandna <chandnamanan2004@gmail.com>
2025-01-21 14:15:14 +0530

    dataconfig.py

---
+++ Machine_learning/Sagemaker_clarify/dataconfig.py
@@ -0,0 +1,8 @@
+data_config = {
+    "s3_data_input_path": "s3://manan-data-lake-bucket/cleaned-data/",
+    "s3_output_path": "s3://manan-data-lake-bucket/explainability-results/",
+    "label": "sentiment",
+    "features": [
+        "text_embedding1", "text_embedding2", "text_embedding3", ...
+    ]
+}
